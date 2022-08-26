ef1ff2612d6cdbb93ed16ae2af51bd0328b4672d
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-08-26 16:57:35 +0530

    delete backend tf file

--- addon/nginx-ingress/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt = true
-    bucket  = "tf-backend-tikal-task"
-    key     = "tf-state-nginx-ingress/terraform.tfstate"
-    region  = "eu-central-1"
-  }
-}    
